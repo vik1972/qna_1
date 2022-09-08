b711247aced016d3449cae861e2887c04aab5586
vik1972 <bvv33@yandex.ru>
2022-09-08 14:26:49 +0200

    Added GET #new to QuestionsController with specs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,8 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @question = Question.new
+  end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+  
   describe "GET #index" do
     let(:questions){ create_list(:question, 3) }
     before  {get :index}
@@ -15,7 +17,6 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe "GET #show" do
-    let(:question) { create(:question) }
     before {get :show, params: { id: question } }
 
     it 'assigns the requested question to @question' do
@@ -26,4 +27,17 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :show
     end
   end
+
+  describe 'GET #new' do
+    before {get :new }
+
+    it 'assigns the new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new vie' do
+      expect(response).to render_template :new
+    end
+  end
+
 end
